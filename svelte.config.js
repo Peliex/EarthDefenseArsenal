3ef98d20012679c8a8eef7304e25bd1def8470ac
Niko <20830349+Peliex@users.noreply.github.com>
2024-08-02 13:13:50 -0700

    updated config

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,12 @@ export default {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
     }
 };
