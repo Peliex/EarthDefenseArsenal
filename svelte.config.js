c3f124fc081bb550706df654c440bc81c423ea18
Niko <20830349+Peliex@users.noreply.github.com>
2024-08-02 00:37:45 -0700

    trying a thing

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 
     kit: {
 		adapter: adapter({
-			fallback: '200.html'
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/EDA' :'',
